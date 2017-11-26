b384bd7d30de219f49a6913a451d9a5f1db4533b
An Nguyen <nthienan.it@gmail.com>
2017-11-26 18:26:33 +0700

    edit web GUI

--- webapp/libs/loginCtrl.js
+++ webapp/libs/loginCtrl.js
@@ -2,47 +2,49 @@
  * @author nthienan
  */
 // login controller
-mainApp.controller('loginCtrl', function($rootScope, $scope, $http, $location, $cookieStore, ngProgress, $translate, langService) {
-	$scope.rememberMe = true;
-	$scope.haveError = false;
-	
-	$scope.showSignUp = function() {
-		$location.path("/sign-up");
-	}
-	
-	$scope.login = function() {
-		ngProgress.start();
-		$http({
-			method: 'POST',
-			url: '/api/user/authenticate/' + $scope.username + '/' + $scope.password,
-			headers : {'Content-Type': 'application/x-www-form-urlencoded'},
-		})
-		.success(function(data, status) {   
-			var authToken = data.token;
-			$scope.haveError = false;
-			$rootScope.authToken = authToken;
-			
-			$cookieStore.put('authToken', authToken);
-			$http.get('/api/user/get')
- 				.success(function(data){
- 					$rootScope.user = data.response;
- 					$rootScope.authenticated = true;
- 					ngProgress.complete();
- 					$location.path("/");
- 				})
- 				.error(function(data, status){
- 					ngProgress.complete();
- 				});
-		})
-		.error(function(data, status) {
-			$scope.haveError = true;
-			$rootScope.error = "Username/password is incorect";
-			ngProgress.complete();
-		});
-	};
-	
-	$scope.$on('langBroadcast', function() {
-		$translate.use(langService.key);
-		$scope.lang = langService.key;
+mainApp.controller('loginCtrl', function ($rootScope, $scope, $http, $location, $cookieStore, ngProgress, $translate, langService, globalConfig) {
+    $scope.rememberMe = false;
+    $scope.haveError = false;
+
+    $scope.showSignUp = function () {
+        $location.path("/sign-up");
+    }
+
+    $scope.login = function () {
+        ngProgress.start();
+        $http({
+            method: 'POST',
+            url: globalConfig.baseURL + '/login',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({email: $scope.username, password: $scope.password})
+        }).success(function (data, status) {
+            var authToken = data.token;
+            $scope.haveError = false;
+            $rootScope.authToken = authToken;
+
+            $cookieStore.put('authToken', authToken);
+            $http.get(globalConfig.baseURL + '/user/me')
+                .success(function (data) {
+                    $rootScope.user = data;
+                    $rootScope.authenticated = true;
+                    ngProgress.complete();
+                    $location.path("/");
+                })
+                .error(function (data, status) {
+                    ngProgress.complete();
+                });
+        })
+            .error(function (data, status) {
+                $scope.haveError = true;
+                $rootScope.error = "Username/password is incorect";
+                ngProgress.complete();
+            });
+    };
+
+    $scope.$on('langBroadcast', function () {
+        $translate.use(langService.key);
+        $scope.lang = langService.key;
     });
 });
--- webapp/libs/songCtrl.js
+++ webapp/libs/songCtrl.js
@@ -2,149 +2,143 @@
  * @author nthienan
  */
 // song controller
-mainApp.controller('songCtrl', function($rootScope, $scope, $http, $location, $filter, ngProgress, $translate, langService) {
-	$scope.selectedId = [];
-	$http.defaults.headers.post['Content-Type'] = 'application/json';
-	
-	// sort
-	var orderBy = $filter('orderBy');
-	$scope.order = function(predicate, reverse) {
-		$rootScope.songResponse.content = orderBy($rootScope.songResponse.content, predicate, reverse);
-		switch(predicate){
-			case 'id': 
-				$scope.id = reverse ? 'glyphicon-sort-by-attributes-alt' : 'glyphicon-sort-by-attributes';
-				$scope.name = '';
-				$scope.gener = '';
-				$scope.musician = '';
-				$scope.artist = '';
-				break;
-			case 'name': 
-				$scope.name = reverse ? 'glyphicon-sort-by-attributes-alt' : 'glyphicon-sort-by-attributes';
-				$scope.id = '';
-				$scope.gener = '';
-				$scope.musician = '';
-				$scope.artist = '';
-				break;
-			case 'gener': 
-				$scope.gener = reverse ? 'glyphicon-sort-by-attributes-alt' : 'glyphicon-sort-by-attributes';
-				$scope.name = '';
-				$scope.id = '';
-				$scope.musician = '';
-				$scope.artist = '';
-				break;
-			case 'musician': 
-				$scope.musician = reverse ? 'glyphicon-sort-by-attributes-alt' : 'glyphicon-sort-by-attributes';
-				$scope.name = '';
-				$scope.id = '';
-				$scope.gener = '';
-				$scope.artist = '';
-				break;
-			case 'artist': 
-				$scope.artist = reverse ? 'glyphicon-sort-by-attributes-alt' : 'glyphicon-sort-by-attributes';
-				$scope.name = '';
-				$scope.id = '';
-				$scope.gener = '';
-				$scope.musician = '';
-				break;
-		}
-	};
+mainApp.controller('songCtrl', function ($rootScope, $scope, $http, $location, $filter, ngProgress, $translate, langService, globalConfig) {
+        $scope.selectedId = [];
+        $http.defaults.headers.post['Content-Type'] = 'application/json';
 
-	// load list
-	$scope.load = function() {
-		ngProgress.start();
-		$http.get('/api/song/' + $rootScope.user.username + '?page=' + ($rootScope.pageNumber - 1) + '&size=' + $rootScope.pageSize)
-			.success(function(data, status) {
-				$rootScope.songResponse = data.response;
-				ngProgress.complete();
-			})
-			.error(function(data, status){
-				console.log(status + data);
-				ngProgress.complete();
-			});
-	};
+        // sort
+        var orderBy = $filter('orderBy');
+        $scope.order = function (predicate, reverse) {
+            $rootScope.songResponse.content = orderBy($rootScope.songResponse.content, predicate, reverse);
+            switch (predicate) {
+                case 'id':
+                    $scope.id = reverse ? 'glyphicon-sort-by-attributes-alt' : 'glyphicon-sort-by-attributes';
+                    $scope.name = '';
+                    $scope.gener = '';
+                    break;
+                case 'name':
+                    $scope.name = reverse ? 'glyphicon-sort-by-attributes-alt' : 'glyphicon-sort-by-attributes';
+                    $scope.id = '';
+                    $scope.gener = '';
+                    break;
+                case 'gener':
+                    $scope.gener = reverse ? 'glyphicon-sort-by-attributes-alt' : 'glyphicon-sort-by-attributes';
+                    $scope.name = '';
+                    $scope.id = '';
+                    break;
+            }
+        };
 
-	// save a song into database
-	$scope.create = function() {
-		ngProgress.start();
-		if($scope.formData.shared == undefined)
-			$scope.formData.shared = false;
-		var formData = new FormData();
-		formData.append("name", $scope.formData.name);
-		formData.append("gener", $scope.formData.gener);
-		formData.append("artist", $scope.formData.artist);
-		formData.append("shared", $scope.formData.shared);
-		formData.append("musician", $scope.formData.musician);
-		formData.append("file", document.forms['formUpload'].file.files[0]);
-		$http({
-		        method: 'POST',
-		        url: '/api/song/' + $rootScope.user.username + '/upload',
-		        headers: {'Content-Type': undefined},
-		        data: formData
-	     })
-	    .success(function(data, status) {   
-	    	$location.path('/');
-	     })
-	     .error(function(data, status){
-	    	 ngProgress.complete();
-	     });
-	};
-	
-	// delete
-	$scope.deleteMulti = function(){
-		ngProgress.start();
-		var values = JSON.stringify($scope.selectedId);
-		jQuery.ajax({
-			headers : {
-				'Accept' : 'application/json',
-				'Content-Type' : 'application/json'},
-				'type' : 'DELETE',
-				'url' : '/api/song/' + $rootScope.user.username,
-				'data' : values,
-				'dataType' : 'json'
-			})
-			.success(function(data){
-				ngProgress.complete();
-				$scope.load();
-			})
-			.error(function(data, status){
-				ngProgress.complete();
-			});
-	};
-	
-	// play
-	$scope.playSong = function(id) {
-		ngProgress.start();
-		$http.put('/api/song/' + id + '/view')
-			.success(function(data) {
-				ngProgress.complete();
-				$location.path('/play-song/' + id);
-			})
-			.error(function(data, status){
-				ngProgress.complete();
-			});
-	};
-	
-	// push or splice selected id
-	$scope.select = function(id){
-		var idx = $scope.selectedId.indexOf(id);
-		if(idx > -1)
-			$scope.selectedId.splice(idx, 1);
-		else
-			$scope.selectedId.push(id);
-	};
+        // load list
+        $scope.load = function () {
+            ngProgress.start();
+            $http.get(globalConfig.baseURL + '/' + $rootScope.user.email + '/song' + '?page=' + ($rootScope.pageNumber - 1) + '&size=' + $rootScope.pageSize)
+                .success(function (data) {
+                    $rootScope.songResponse = data;
+                    ngProgress.complete();
+                })
+                .error(function (data, status) {
+                    console.log(status + data);
+                    ngProgress.complete();
+                });
+        };
 
-	// redirect to edit view
-	$scope.editSong = function(id) {
-		$location.path('/edit-song/' + id);
-	};
-	
-	$scope.back = function() {
-		$location.path('/');
-	};
-	
-	$scope.$on('langBroadcast', function() {
-		$translate.use(langService.key);
-    });
-	
-	$scope.load();
-});
+        $scope.create = function () {
+            ngProgress.start();
+            if ($scope.shared == undefined)
+                $scope.shared = false;
+            reader = new FileReader();
+            reader.onload = function () {
+                var f = new Uint8Array(reader.result);
+                $http({
+                    method: 'POST',
+                    url: globalConfig.baseURL + '/song/upload',
+                    headers: {'Content-Type': "application/octet-stream"},
+                    data: f,
+                    transformRequest: []
+                }).success(function (data) {
+                    $http({
+                        method: 'POST',
+                        url: globalConfig.baseURL + '/song',
+                        headers: {'Content-Type': "application/json"},
+                        data: JSON.stringify({
+                            name: $scope.name,
+                            genre: $scope.genre,
+                            file: data.path,
+                            shared: $scope.shared,
+                            owner: $rootScope.user.email
+                        })
+                    }).success(function (data) {
+                        ngProgress.complete();
+                        $location.path("/");
+                    }).error(function (data, status) {
+                        ngProgress.complete();
+                    })
+                }).error(function (data, status) {
+                    ngProgress.complete();
+                });
+            };
+            reader.readAsArrayBuffer(document.forms['formUpload'].file.files[0]);
+        };
+
+        // delete
+        $scope.deleteMulti = function () {
+            ngProgress.start();
+            var values = JSON.stringify($scope.selectedId);
+            jQuery.ajax({
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                'type': 'DELETE',
+                'url': '/api/song/' + $rootScope.user.username,
+                'data': values,
+                'dataType': 'json'
+            })
+                .success(function (data) {
+                    ngProgress.complete();
+                    $scope.load();
+                })
+                .error(function (data, status) {
+                    ngProgress.complete();
+                });
+        };
+
+        // play
+        $scope.playSong = function (id) {
+            ngProgress.start();
+            $http.put('/api/song/' + id + '/view')
+                .success(function (data) {
+                    ngProgress.complete();
+                    $location.path('/play-song/' + id);
+                })
+                .error(function (data, status) {
+                    ngProgress.complete();
+                });
+        };
+
+        // push or splice selected id
+        $scope.select = function (id) {
+            var idx = $scope.selectedId.indexOf(id);
+            if (idx > -1)
+                $scope.selectedId.splice(idx, 1);
+            else
+                $scope.selectedId.push(id);
+        };
+
+        // redirect to edit view
+        $scope.editSong = function (id) {
+            $location.path('/edit-song/' + id);
+        };
+
+        $scope.back = function () {
+            $location.path('/');
+        };
+
+        $scope.$on('langBroadcast', function () {
+            $translate.use(langService.key);
+        });
+
+        $scope.load();
+    }
+);
